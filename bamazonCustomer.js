fad6ea83bd725b7a646c1d8900d3681e040d930c
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-07-13 20:58:31 -0400

    trying to add the updateorder function to return a response of insufficient quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -35,7 +35,70 @@ var showItems = function (){
 				);
 		}
 		console.log(showTable.toString());
-		// purchasePrompt();
+		purchasePrompt();
 	});
 }
 showItems()
+function purchasePrompt(){
+	inquirer.prompt([
+	{
+		name: "ID",
+		type: "input",
+		message:"Please enter the Item ID you would like to purhcase.",
+		filter:Number
+	},
+	{
+		name:"Quantity",
+		type:"input",
+		message:"How many of this items would you like to buy?",
+		filter:Number
+	},
+
+ ]).then(function(answers){
+ 	var quantityNeeded = answers.Quantity;
+ 	var IDrequested = answers.ID;
+ 	purchaseOrder(IDrequested, quantityNeeded);
+ });
+};
+
+function purchaseOrder(ID, itemAmount){
+	connection.query('Select * FROM items WHERE item_id = ' + ID, function(err,res){
+		if(err){console.log(err)};
+		if(itemAmount <= res[0].stock_quantity){
+			var totalCost = res[0].price * itemAmount;
+			console.log("Great news your order is in stock!");
+			console.log("Your total cost for " + itemAmount + " " +res[0].product_name + " is" + " $" + totalCost + ". Thank you!");
+
+			// connection.query("UPDATE items SET stock_quantity = stock_quantity - " + itemAmount + "WHERE ? " + ID);
+        } 
+        // else{
+		// 	console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
+		// };
+        // showItems();
+        updateOrder()
+	});
+};
+
+function updateOrder(res) {
+    // if (err) throw (err);
+    console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
+    var query = connection.query(
+        console.log(query.sql)
+    //   "UPDATE items SET ? WHERE ?",
+    //   [
+    //     {
+    //       stock_quantity: []
+    //     },
+    //     {
+    //       product_name: []
+    //     }
+    // //   ],
+    //   function(err, res) {
+    //     if (err) throw err;
+    //     console.log("sorry we do not have enough " + res[0].product_name + "to complete your order.");
+
+    //   }
+    );
+   
+}
+  
